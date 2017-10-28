2cc770dd5d35ed73c8f4c7f673d9db45bc9a77b2
aliona95 <alionalisovskaja95@gmail.com>
2017-10-29 00:00:52 +0300

    for tests

--- ForwardChaining/src/Main.java
+++ ForwardChaining/src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,13 +9,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-   private static String filename = "file1.txt";
-    static ArrayList<Production> productions = new ArrayList<Production>();
-    static List<String> facts;
-    static char goal;
-    static String production;
-    static char consistent;
-    static ArrayList<String> antecedents;
+    private static String filename = "file1.txt";
+    private static ArrayList<Production> productions = new ArrayList<Production>();
+    private static List<String> facts;
+    private static String goal;
+    private static String production;
+    private static char consistent;
+    private static ArrayList<String> antecedents;
+    private static ArrayList<String> GDB = new ArrayList<String>();
+    private static boolean isFlag1 = false;
+    private static int counter = 0;
+    private static String missingAntecedent = "";
+    private static Boolean consistentInFacts = false;
+    private static Boolean isFlag2 = false;
+    private static ArrayList<Integer> path = new ArrayList<Integer>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -27,9 +36,10 @@ public class Main {
                 if (line.equals("2) Faktai")){
                     line = br.readLine();
                     facts = new ArrayList<String>(Arrays.asList(line.split(" ")));
+                    GDB = (ArrayList<String>) facts;
                 }else if (line.equals("3) Tikslas")){
                     line = br.readLine();
-                    goal = line.charAt(0);
+                    goal = String.valueOf(line.charAt(0));
                 }else{
                     // getting all productions
                     production = line.substring(0, line.indexOf("/"));
@@ -45,24 +55,98 @@ public class Main {
         System.out.println();
         System.out.println("  1) Taisyklės");
         for(int i = 0; i < productions.size(); i++){
-            System.out.println("     R" + (i + 1) + ": " + productions.get(i).printAntecendents() + "-> " + productions.get(i).getConsistent());
+            System.out.println("     R" + (i + 1) + ": " + productions.get(i).printAntecendents() + " -> " + productions.get(i).getConsistent());
         }
         System.out.println();
         System.out.println("  2) Faktai");
         System.out.print("     ");
         printFacts();
-        System.out.println();
+        System.out.println("\n");
         System.out.println("  3) Tikslas");
         System.out.print("     " + goal + "\n\n");
         System.out.println("2 DALIS. Vykdymas");
-        System.out.println();
+        forwardChaining();
+        if(GDB.contains(goal)) {
+            System.out.println("   Tikslas gautas.");
+            System.out.println("\n3 DALIS. Rezultatai");
+            System.out.println("    1) Tikslas " + goal + " išvestas.");
+            System.out.print("    2) Kelias: ");
+        }else{
+            System.out.println("   Tikslas negautas.");
+        }
+        for (int i = 0; i < path.size(); i++) {
+            if (i != path.size() - 1) {
+                System.out.print("R" + path.get(i) + ", ");
+            } else {
+                System.out.print("R" + path.get(i) + ".");
+            }
+        }
     }
 
     public static void printFacts(){
         for(int i = 0; i < facts.size() - 1; i++){
             System.out.print(facts.get(i) + ", ");
         }
-        System.out.print(facts.get(facts.size() - 1) + " \n");
+        System.out.print(facts.get(facts.size() - 1));
     }
 
+    public static void forwardChaining(){
+        if((!GDB.contains(goal))) {
+            System.out.println("\n " + (++counter) + " ITERACIJA");
+            for (int i = 0; i < productions.size(); i++) {
+                if ((!GDB.contains(goal))) {
+                    isFlag1 = false;
+                    isFlag2 = false;
+                    consistentInFacts = false;
+                    if (canApply(i)) {
+                        productions.get(i).setFlag1(true);  // pakeliame - true
+                        GDB.add(String.valueOf(productions.get(i).getConsistent()));
+                        System.out.print("   R" + (i + 1) + ":" + productions.get(i).printAntecendents() + "->" + productions.get(i).getConsistent() + " taikome. Pakeliame flag1. Faktai ");
+                        printFacts();
+                        System.out.println(".");
+                        path.add(i + 1);
+                        forwardChaining();
+                    } else if (isFlag1) {
+                        System.out.println("   R" + (i + 1) + ":" + productions.get(i).printAntecendents() + "->" + productions.get(i).getConsistent() + " praleidžiame, nes pakelta flag1.");
+                    } else if (consistentInFacts) {
+                        System.out.println("   R" + (i + 1) + ":" + productions.get(i).printAntecendents() + "->" + productions.get(i).getConsistent() + " netaikome, nes konsekventas faktuose. Pakeliame flag2.");
+                    } else if (isFlag2) {
+                        System.out.println("   R" + (i + 1) + ":" + productions.get(i).printAntecendents() + "->" + productions.get(i).getConsistent() + " praleidžiame, nes pakelta flag2.");
+                    } else {
+                        System.out.println("   R" + (i + 1) + ":" + productions.get(i).printAntecendents() + "->" + productions.get(i).getConsistent() + " netaikome, nes trūksta " + missingAntecedent + ".");
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean canApply(int i){   // FLAGUS TIKRINTI
+        int counter = 0;
+        for(int j = 0; j < productions.get(i).getAntecedents().size(); j++){
+            if(productions.get(i).getFlag1()){
+                isFlag1 = true;
+                return false;
+            }
+            if(productions.get(i).getFlag2()){
+                isFlag2 = true;
+                return false;
+            }
+            if(GDB.contains(productions.get(i).getAntecedents().get(j))) {
+                counter++;
+            }else{
+                //System.out.println("TRUKSTA " + productions.get(i).getAntecedents().get(j));
+                missingAntecedent = productions.get(i).getAntecedents().get(j);
+                return false;
+            }
+            if(facts.contains(String.valueOf(productions.get(i).getConsistent()))){
+                consistentInFacts = true;
+                productions.get(i).setFlag2(true);
+                return false;
+            }
+        }
+        if(counter == productions.get(i).getAntecedents().size()){
+            return true;
+        }
+        return false;
+    }
 }
--- ForwardChaining/src/Production.java
+++ ForwardChaining/src/Production.java
@@ -6,23 +6,45 @@ public class Production {
     private char consistent;
     private ArrayList<String> antecedents;
     private boolean flag1 = false;
+    private boolean flag2 = false;
 
     Production(char consistent, ArrayList<String> antecedents){
         this.consistent = consistent;
         this.antecedents = antecedents;
         flag1 = false;
+        flag2 = false;
     }
 
     public String printAntecendents(){
         String text = "";
         for(int i = 0; i < antecedents.size() - 1; i++){
-            text += antecedents.get(i) + ", ";
+            text += antecedents.get(i) + ",";
         }
-        text += antecedents.get(antecedents.size() - 1) + " ";
+        text += antecedents.get(antecedents.size() - 1);
         return text;
     }
 
     public char getConsistent(){
         return consistent;
+    }
+
+    public ArrayList<String> getAntecedents(){
+        return antecedents;
+    }
+
+    public void setFlag1(boolean flag1){
+        this.flag1 = flag1;
+    }
+
+    public boolean getFlag1(){
+        return flag1;
+    }
+
+    public void setFlag2(boolean flag2){
+        this.flag2 = flag2;
+    }
+
+    public boolean getFlag2(){
+        return flag2;
     }
 }
